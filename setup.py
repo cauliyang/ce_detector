ca518914d1b4b589751ff77623dd1fa2e90049c0
YangyangLI <li002252@umn.edu>
2021-01-15 18:51:02 +0800

    path version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/cauliyang/ce_detector/"
 EMAIL = "li002252@umn.edu"
 AUTHOR = "YangyangLi"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.1.0"
+VERSION = "version='0.1.1'"
 
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
