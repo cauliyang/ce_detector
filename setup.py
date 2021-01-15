288625535ba2c0380e429dc2d3a1cf22ffa709c2
YangyangLI <li002252@umn.edu>
2021-01-15 19:49:33 +0800

    change version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/cauliyang/ce_detector/"
 EMAIL = "li002252@umn.edu"
 AUTHOR = "YangyangLi"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "version=0.1.4"
+VERSION = "version=0.1.5"
 
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
